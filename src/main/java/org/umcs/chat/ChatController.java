deb76dd8ee3585eb7af8119235d61778804795d8
VasylIshchuk <Vasyl.Ishchuk@live.umcs.edu.pl>
2024-06-16 00:34:26 +0200

    Task 1-3

--- src/main/java/org/umcs/chat/ChatController.java
+++ src/main/java/org/umcs/chat/ChatController.java
@@ -1,4 +1,35 @@
 package org.umcs.chat;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ChatController {
+    @FXML
+    public TextArea textArea;
+    @FXML
+    public TextField textField;
+
+    public ConnectionHandler client;
+//  This field is need to send a message to the server
+    public void initialize() {
+        ClientReceiver.controller = this;
+    }
+//    This method is automatically called after the FXML file is loaded(Scene scene = new Scene(fxmlLoader.load());).
+//    We use it to set the controller in the ClientReceiver.
+    @FXML
+    public void onSendMessage(){
+        String message = textField.getText();
+        textField.clear();
+        onReceiveMessage(message);
+        client.send(message);
+        if (message.equals("EXIT")){
+            Platform.exit();
+            System.exit(0);
+        }
+    }
+    public void onReceiveMessage(String message){
+        textArea.appendText(message + "\n");
+    }
 }
---
+++ src/main/java/org/umcs/chat/ConnectionHandler.java
@@ -0,0 +1,35 @@
+package org.umcs.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ConnectionHandler implements  Runnable{
+    BufferedReader reader;
+    PrintWriter writer;
+
+    public ConnectionHandler(Socket clientSocket) throws IOException {
+        reader = new BufferedReader(
+                new InputStreamReader(
+                        clientSocket.getInputStream()
+                ));
+        writer = new PrintWriter(clientSocket.getOutputStream(),true);
+    }
+
+    @Override
+    public void run() {
+        String message;
+        try {
+            while((message = reader.readLine())!= null) {
+                ClientReceiver.receive(message);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void send(String message){
+        writer.println(message);
+    }
+}
